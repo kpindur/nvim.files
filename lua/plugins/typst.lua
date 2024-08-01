81a51ef518d742e7a6aa28bc57aaf003bb0ccb25
kpindur <kotaro.pindur@gmail.com>
2024-08-01 18:33:09 +0900

    feat(typst): add typst support

---
+++ lua/plugins/typst.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'chomosuke/typst-preview.nvim',
+    --lazy = false, -- or ft = 'typst'
+    ft = 'typst',
+    version = '0.3.*',
+    build = function() require 'typst-preview'.update() end,
+  },
+}
